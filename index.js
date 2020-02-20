1a2f8632f1b59a0ee8fae930f5ff67028b7a8b47
Hamza Bishira <bishira12@hotmail.co.uk>
2020-02-20 21:16:27 +0000

    latest update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+let hexNums = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 let backgroundColor = document.querySelector("body");
 let button = document.querySelector("button");
 
+let thehex = document.querySelector(".hex")
 
 button.addEventListener("click", function(){
-  
+  let hexValue = "#"
+  for(let i = 0; i < 6; i++){
+     let random = Math.floor(Math.random() * hexNums.length);
+      hexValue += hexNums[random];
+  }
+  backgroundColor.style.backgroundColor = hexValue;
+  thehex.innerHTML = hexValue;
+
 });
